664cb2570ea4b8acb58a65d497ca3d8e93c30bdf
Kenyansa <kenyansajustine@gmail.com>
2022-04-08 00:23:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const DODGER = document.getElementById("dodger");
+const GAME = document.getElementById("game");
+
+  function moveDodgerRight() {
+  window.requestAnimationFrame(function() {
+    const left = positionToInteger(DODGER.style.left)
+    if (left < 360) {
+      DODGER.style.left = `${left + 4}px`;
+    }
+  });
+  function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
